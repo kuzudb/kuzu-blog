fbafbff978a25ff84c434fb26ac20692b586ee8d
Chang Liu <liuc223@gmail.com>
2024-03-05 18:02:31 +0800

    Address #7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-import compressor from "astro-compressor";
+import compressor from 'astro-compressor';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import { VitePWA } from 'vite-plugin-pwa';
 import rehypeKatex from 'rehype-katex'; // relevant
-import remarkMath from 'remark-math';   // relevant
+import remarkMath from 'remark-math'; // relevant
+import remarkToc from 'remark-toc';
 
 import { manifest } from './src/utils/manifest';
 
@@ -23,14 +24,18 @@ export default defineConfig({
       theme: 'material-theme-palenight',
       wrap: false,
     },
-    remarkPlugins: [
-        remarkMath,
-    ],
+    remarkPlugins: [remarkMath, remarkToc],
     rehypePlugins: [
-        [rehypeKatex, {
-        // Katex plugin options
-        }]
-    ]
+      [
+        rehypeKatex,
+        {
+          // Katex plugin options
+        },
+      ],
+      'rehype-slug',
+      ['rehype-autolink-headings', { behavior: 'append' }],
+      ['rehype-toc', { headings: ['h1', 'h2', 'h3'] }],
+    ],
   },
   integrations: [
     mdx({
